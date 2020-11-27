c33438e5408308e064a678a18c57f372b924e903
Thomas <thomas.ibanez33@gmail.com>
2020-11-27 21:59:59 +0100

    ImGUI Docking

---
+++ ICE/src/GUI/ICEGUI.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Thomas Ibanez on 27.11.20.
+//
+
+#include "ICEGUI.h"
---
+++ ICE/src/GUI/ICEGUI.h
@@ -0,0 +1,15 @@
+//
+// Created by Thomas Ibanez on 27.11.20.
+//
+
+#ifndef ICE_ICEGUI_H
+#define ICE_ICEGUI_H
+
+namespace ICE {
+    class ICEGUI {
+
+    };
+}
+
+
+#endif //ICE_ICEGUI_H
