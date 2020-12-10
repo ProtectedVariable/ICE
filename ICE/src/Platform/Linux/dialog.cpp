790d21135b26b00335ec759b52305bc9d56dcea1
Thomas <thomas.ibanez33@gmail.com>
2020-12-10 13:11:42 +0100

    removed nfd stuff

--- ICE/src/Platform/Linux/dialog.cpp
+++ ICE/src/Platform/Linux/dialog.cpp
@@ -8,10 +8,8 @@
 const std::string open_native_dialog(std::string const& filter) {
 
     GtkWidget *dialog;
-    nfdresult_t result;
 
     if (!gtk_init_check(NULL, NULL)) {
-        NFDi_SetError(INIT_FAIL_MSG);
         return std::string();
     }
 
@@ -22,21 +20,14 @@ const std::string open_native_dialog(std::string const& filter) {
                                          "_Open", GTK_RESPONSE_ACCEPT,
                                          NULL);
 
-    result = NFD_CANCEL;
     if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
         char *filename;
         filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
-
-        result = NFD_OKAY;
-        WaitForCleanup();
         gtk_widget_destroy(dialog);
-        WaitForCleanup();
         return std::string(filename);
     }
 
-    WaitForCleanup();
     gtk_widget_destroy(dialog);
-    WaitForCleanup();
 
     return std::string("");
 }
