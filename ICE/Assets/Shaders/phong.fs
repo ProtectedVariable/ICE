328115bba5e91499caa25037339f7aaede4ade1d
Thomas <thomas.ibanez33@gmail.com>
2020-12-25 11:49:36 +0100

    Diffuse, ambient and specular maps done

--- ICE/Assets/Shaders/phong.fs
+++ ICE/Assets/Shaders/phong.fs
@@ -14,14 +14,14 @@ struct Material {
     vec3 specular;
     vec3 ambient;
     float alpha;
-    bool useDiffuseMap;
-    sampler2D diffuseMap;
-    bool useAmbientMap;
-    sampler2D ambientMap;
-    bool useSpecularMap;
-    sampler2D specularMap;
-    bool useNormalMap;
-    sampler2D normalMap;
+    bool use_diffuse_map;
+    sampler2D diffuse_map;
+    bool use_ambient_map;
+    sampler2D ambient_map;
+    bool use_specular_map;
+    sampler2D specular_map;
+    bool use_normal_map;
+    sampler2D normal_map;
 };
 
 uniform vec3 ambient_light;
@@ -42,8 +42,8 @@ vec3 pointLight(Light light) {
     vec3 light_direction = normalize(light.position - fposition);
     float diff = max(dot(n, light_direction), 0.0);
     vec3 diffuse_color = material.albedo;
-    if(material.useDiffuseMap) {
-        diffuse_color *= texture(material.diffuseMap, ftex_coords).xyz;
+    if(material.use_diffuse_map) {
+        diffuse_color *= texture(material.diffuse_map, ftex_coords).xyz;
     }
     rcolor += light.color * (diff * diffuse_color);
 
@@ -52,8 +52,8 @@ vec3 pointLight(Light light) {
     vec3 reflection_direction = reflect(-light_direction, n);
     float spec = pow(max(dot(view_direction, reflection_direction), 0.0), material.alpha);
     vec3 specular_color = material.specular;
-    if(material.useSpecularMap) {
-        specular_color *= texture(material.specularMap, ftex_coords).xyz;
+    if(material.use_specular_map) {
+        specular_color *= texture(material.specular_map, ftex_coords).xyz;
     }
     rcolor += light.color * (spec * specular_color);
 
@@ -63,16 +63,16 @@ vec3 pointLight(Light light) {
 void main() {
     //ambient
     vec3 color_accumulator = vec3(material.ambient*ambient_light);
-    if(material.useAmbientMap) {
-        color_accumulator *= texture(material.ambientMap, ftex_coords).xyz;
+    if(material.use_ambient_map) {
+        color_accumulator *= texture(material.ambient_map, ftex_coords).xyz;
     }
     for(int i = 0; i < light_count; i++) {
         color_accumulator += pointLight(lights[i]);
     }
     if(light_count == 0) {
         frag_color = vec4(material.albedo, 1.0);
-        if(material.useDiffuseMap) {
-           frag_color *= texture(material.diffuseMap, ftex_coords);
+        if(material.use_diffuse_map) {
+           frag_color *= texture(material.diffuse_map, ftex_coords);
         }
     } else {
         frag_color = vec4(color_accumulator, 1.0);
--- ICE/src/GUI/NewMaterialPane.cpp
+++ ICE/src/GUI/NewMaterialPane.cpp
@@ -35,13 +35,72 @@ namespace ICE {
         ImGui::SameLine();
         ImGui::SetNextItemWidth(ICE_NEWMAT_PICKER_WIDTH);
         ImGui::ColorPicker3("##NewMaterialAmbient", ambient.data(), flags);
-        ImGui::Text("Alpha");
-        ImGui::SameLine();
+
 
         //Maps
-        //ImGui::Combo("##NewMaterialDiffuseMap", )
+        auto textures = std::vector<const char*>(engine->getAssetBank()->getTextures().size() + 1);
+        textures[0] = "None";
+        int i = 1;
+        int selected[4] = {0,0,0,0};
+        const char* mapNames[4] =  {"Diffuse", "Specular", "Ambient", "Normal"};
+        for(const auto &t : engine->getAssetBank()->getTextures()) {
+            textures[i++] = t.first.c_str();
+            if(t.second == diffuseMap) {
+                selected[0] = i-1;
+            }
+            if(t.second == specularMap) {
+                selected[1] = i-1;
+            }
+            if(t.second == ambientMap) {
+                selected[2] = i-1;
+            }
+            if(t.second == normalMap) {
+                selected[3] = i-1;
+            }
+        }
+        ImGui::Image(diffuseMap == nullptr ? 0 : diffuseMap->getTexture(), ImVec2(ICE_NEWMAT_PICKER_WIDTH, ICE_NEWMAT_PICKER_WIDTH), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::SameLine();
+        ImGui::Image(specularMap == nullptr ? 0 : specularMap->getTexture(), ImVec2(ICE_NEWMAT_PICKER_WIDTH, ICE_NEWMAT_PICKER_WIDTH), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::SameLine();
+        ImGui::Image(ambientMap == nullptr ? 0 : ambientMap->getTexture(), ImVec2(ICE_NEWMAT_PICKER_WIDTH, ICE_NEWMAT_PICKER_WIDTH), ImVec2(0, 1), ImVec2(1, 0));
+        ImGui::SameLine();
+        ImGui::Image(normalMap == nullptr ? 0 : normalMap->getTexture(), ImVec2(ICE_NEWMAT_PICKER_WIDTH, ICE_NEWMAT_PICKER_WIDTH), ImVec2(0, 1), ImVec2(1, 0));
+
+        for(int i = 0; i < 4; i++) {
+            ImGui::Text("%s Map", mapNames[i]);
+            ImGui::SameLine();
+            ImGui::SetNextItemWidth(ICE_NEWMAT_PICKER_WIDTH);
+            ImGui::Combo(("##NewMaterial"+std::string(mapNames[i])+"Map").c_str(), &selected[i], textures.data(), engine->getAssetBank()->getTextures().size() + 1, 10);
+            if(i < 3)
+                ImGui::SameLine();
+        }
+
+        for(int i = 0; i < 4; i++) {
+            const Texture** map = nullptr;
+            switch(i) {
+                case 0:
+                    map = &diffuseMap;
+                    break;
+                case 1:
+                    map = &specularMap;
+                    break;
+                case 2:
+                    map = &ambientMap;
+                    break;
+                case 3:
+                    map = &normalMap;
+                    break;
+            }
+            if(selected[i] > 0) {
+                *map = engine->getAssetBank()->getTexture(textures[selected[i]]);
+            } else {
+                *map = nullptr;
+            }
+        }
 
         //Alpha
+        ImGui::Text("Alpha");
+        ImGui::SameLine();
         ImGui::SliderFloat("##NewMaterialAlpha", &alpha, 0, 100);
 
         //Preview
@@ -54,6 +113,18 @@ namespace ICE {
         engine->getApi()->clear();
         Shader* shader = engine->getAssetBank()->getShader("__ice__phong_shader");
         shader->bind();
+        if(diffuseMap != nullptr) {
+            diffuseMap->bind(0);
+        }
+        if(specularMap != nullptr) {
+            specularMap->bind(1);
+        }
+        if(ambientMap != nullptr) {
+            ambientMap->bind(2);
+        }
+        if(normalMap != nullptr) {
+            normalMap->bind(3);
+        }
         camera.setParameters({50, wsize.x / wsize.y, 0.01f, 1000});
         shader->loadMat4("projection", camera.getProjection());
         shader->loadMat4("view", camera.lookThrough());
@@ -66,6 +137,14 @@ namespace ICE {
         shader->loadFloat3("material.specular", specular);
         shader->loadFloat3("material.ambient",ambient);
         shader->loadFloat("material.alpha", alpha);
+        shader->loadInt("material.use_diffuse_map", diffuseMap != nullptr);
+        shader->loadInt("material.diffuse_map", 0);
+        shader->loadInt("material.use_specular_map", specularMap != nullptr);
+        shader->loadInt("material.specular_map", 1);
+        shader->loadInt("material.use_ambient_map", ambientMap != nullptr);
+        shader->loadInt("material.ambient_map", 2);
+        shader->loadInt("material.use_normal_map", normalMap != nullptr);
+        shader->loadInt("material.normal_map", 3);
         engine->getApi()->renderVertexArray(engine->getAssetBank()->getMesh("__ice__sphere")->getVertexArray());
         viewFB->unbind();
         ImGui::Image(viewFB->getTexture(), wsize, ImVec2(0, 1), ImVec2(1, 0));
@@ -97,7 +176,7 @@ namespace ICE {
 
     void NewMaterialPane::build() {
         if(!editMode) {
-            auto mtl = Material(engine->getAssetBank()->getShader("__ice__phong_shader"), albedo, specular, ambient, alpha);
+            auto mtl = Material(engine->getAssetBank()->getShader("__ice__phong_shader"), albedo, specular, ambient, alpha, diffuseMap, specularMap, ambientMap, normalMap);
             engine->getAssetBank()->addMaterial(name, mtl);
         } else {
             std::string newName = oldname;
