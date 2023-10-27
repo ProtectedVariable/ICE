c4cca5a1441fefbc98863638ca1fc2169ca1b590
Thomas Ibanez <thomas.ibanez@earlysight.com>
2023-10-27 11:11:16 +0200

    remove imxml :(

--- ICEBERG/UI/ProjectSelectionWindow.h
+++ ICEBERG/UI/ProjectSelectionWindow.h
@@ -1,52 +1,10 @@
-#include <XMLDynamicBind.h>
-#include <XMLEventHandler.h>
-#include <XMLReader.h>
-#include <XMLRenderer.h>
-#include <XMLTree.h>
-
 #include "UIElement.h"
-
-class Handler : public ImXML::XMLEventHandler {
-   public:
-    virtual void onNodeBegin(ImXML::XMLNode& node) override {
-        if (node.args["id"] == "window") {
-            ImGuiViewport* viewport = ImGui::GetMainViewport();
-            ImGui::SetNextWindowPos(viewport->Pos);
-            ImGui::SetNextWindowSize(viewport->Size);
-        }
-    }
-
-    virtual void onNodeEnd(ImXML::XMLNode& node) override {
-        if (node.args["id"].starts_with("pr")) {
-            if (ImGui::IsItemHovered()) {
-                ImGui::TableSetBgColor(ImGuiTableBgTarget_CellBg, ImGui::GetColorU32(ImVec4(0.2, 0.2, 0.2, 1)));
-            }
-            if (ImGui::IsItemClicked()) {
-                //TODO Load project and go to editor
-            }
-        }
-    }
-
-    virtual void onEvent(ImXML::XMLNode& node) override {
-        if (node.args["id"] == "btn0") {
-            //Open file dialog
-        }
-    }
-
-   private:
-    std::string hovered_id = "__NO_ID";
-};
 
 class ProjectSelectionWindow : public UIElement {
    public:
-    ProjectSelectionWindow() { renderer.addDynamicBind("project_name", {project_name, 512, ImXML::XMLDynamicBindType::Chars}); }
+    ProjectSelectionWindow() {}
 
-    void render() override { renderer.render(tree, handler); }
+    void render() override {}
 
    private:
-    ImXML::XMLReader reader;
-    ImXML::XMLTree tree = reader.read("XML/ProjectSelection.xml");
-    ImXML::XMLRenderer renderer;
-    char project_name[512] = {0};
-    Handler handler;
 };
