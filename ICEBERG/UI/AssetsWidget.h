8f732f4afed871cfcddb8f0fd245b34bde3d507e
Thomas <thomas.ibanez@epfl.ch>
2023-10-30 20:00:17 +0100

    basic skeleton of asset view

--- ICEBERG/UI/AssetsWidget.h
+++ ICEBERG/UI/AssetsWidget.h
@@ -1,7 +1,17 @@
 #pragma once
 #include <ImGUI/imgui.h>
 
+#include <memory>
+#include <string>
+#include <vector>
+
 #include "Widget.h"
+
+struct AssetView {
+    std::string folder_name;
+    std::vector<std::string> assets_names;
+    std::vector<std::shared_ptr<AssetView>> subfolders;
+};
 
 class AssetsWidget : public Widget {
    public:
@@ -13,9 +23,65 @@ class AssetsWidget : public Widget {
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
         ImGui::Begin("Assets", 0, flags);
 
+        if (ImGui::BeginTable("asset_layout", 3, ImGuiTableFlags_Resizable)) {
+            ImGui::TableSetupColumn("Folders", ImGuiTableColumnFlags_WidthStretch, 0.15);  
+            ImGui::TableSetupColumn("Content", ImGuiTableColumnFlags_WidthStretch, 0.6);  
+            ImGui::TableSetupColumn("Preview", ImGuiTableColumnFlags_WidthStretch, 0.25);       
+            ImGui::TableHeadersRow();
+            ImGui::TableNextColumn();
+            //Left hand side: Assets folders
+            if (ImGui::BeginTable("asset_folders", 1, ImGuiTableFlags_BordersInnerH)) {
+                for (int i = 0; i < m_assets.size(); i++) {
+                    ImGui::TableNextColumn();
+                    const auto& asset = m_assets[i];
+                    if (ImGui::Selectable(asset->folder_name.c_str(), i == m_selected_index)) {
+                        m_selected_index = i;
+                        m_current_view = m_assets[i];
+                    }
+                }
+                ImGui::EndTable();
+            }
+
+            //Middle: assets
+            ImGui::TableNextColumn();
+            if (ImGui::BeginTable("assets_selection", 10)) {
+                for (int i = 0; i < m_current_view->subfolders.size(); i++) {
+                    const auto& folder = m_current_view->subfolders[i];
+                    ImGui::TableNextColumn();
+                    ImGui::Text(folder->folder_name.c_str());
+                    if (ImGui::IsItemHovered() && ImGui::IsMouseDoubleClicked(0)) {
+                        m_current_view = folder;
+                    }
+                }
+
+                for (int i = 0; i < m_current_view->assets_names.size(); i++) {
+                    const auto& name = m_current_view->assets_names[i];
+                    ImGui::TableNextColumn();
+                    ImGui::Text(name.c_str());
+                }
+                ImGui::EndTable();
+            }
+
+            //Right hand side: preview
+            ImGui::TableNextColumn();
+            ImGui::Text("Preview");
+
+            ImGui::EndTable();
+        }
         ImGui::End();
         ImGui::PopStyleVar();
     }
 
+    void addAssets(const AssetView& assets) {
+        auto ptr = std::make_shared<AssetView>(assets);
+        m_assets.push_back(ptr);
+        if (m_current_view == nullptr) {
+            m_current_view = ptr;
+        }
+    }
+
    private:
+    std::vector<std::shared_ptr<AssetView>> m_assets;
+    int m_selected_index = 0;
+    std::shared_ptr<AssetView> m_current_view = nullptr;
 };
--- ICEBERG/src/Assets.cpp
+++ ICEBERG/src/Assets.cpp
@@ -1,6 +1,9 @@
 #include "Assets.h"
 
 Assets::Assets(const std::shared_ptr<ICE::ICEEngine>& engine) : m_engine(engine) {
+    ui.addAssets({"Mesh", {"Mesh0", "Mesh1", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "Mesh2", "MeshN"}});
+    AssetView sub_1 = {"Subfolder1", {"M1"}, {}};
+    ui.addAssets({"Materials", {"Material0", "Material1", "Mesh2"}, {std::make_shared<AssetView>(sub_1)}});
 }
 
 bool Assets::update() {
