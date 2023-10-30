fd5578bb02622baecc25dd9c17f8f959d79d2f76
Thomas <thomas.ibanez@epfl.ch>
2023-10-30 21:49:44 +0100

    basic hierarchy

--- ICEBERG/UI/HierarchyWidget.h
+++ ICEBERG/UI/HierarchyWidget.h
@@ -1,21 +1,51 @@
 #pragma once
 #include <ImGUI/imgui.h>
 
+#include <memory>
+#include <string>
+#include <vector>
+//#include <Entity.h>
+
 #include "Widget.h"
+
+enum class EntityType { LightSource, Renderable, Camera, Scene, None };
+
+struct SceneTreeView {
+    //ICE::Entity id; Ideally not needed, tbd
+    std::string entity_name;
+    EntityType type = EntityType::None;
+    std::vector<std::shared_ptr<SceneTreeView>> children;
+};
 
 class HierarchyWidget : public Widget {
    public:
     HierarchyWidget() = default;
+
+    void setSceneTree(const SceneTreeView& tree) { m_view = tree; }
 
     void render() override {
         int flags = ImGuiWindowFlags_NoCollapse;
         flags |= ImGuiWindowFlags_NoNavFocus;
         ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
         ImGui::Begin("Hierarchy", 0, flags);
-        
+
+        renderTree(m_view);
+
         ImGui::End();
         ImGui::PopStyleVar();
     }
 
    private:
+    void renderTree(const SceneTreeView& tree) {
+        auto flags = tree.children.empty() ? ImGuiTreeNodeFlags_Leaf : 0;
+        if (ImGui::TreeNodeEx(tree.entity_name.c_str(), flags)) {
+            for (const auto& c : tree.children) {
+                renderTree(*c);
+            }
+            ImGui::TreePop();
+        }
+    }
+
+   private:
+    SceneTreeView m_view;
 };
--- ICEBERG/src/Hierarchy.cpp
+++ ICEBERG/src/Hierarchy.cpp
@@ -1,6 +1,11 @@
 #include "Hierarchy.h"
 
-Hierarchy::Hierarchy(const std::shared_ptr<ICE::ICEEngine>& engine) : m_engine(engine) {}
+Hierarchy::Hierarchy(const std::shared_ptr<ICE::ICEEngine>& engine) : m_engine(engine) {
+    SceneTreeView subtree1 = {"Entity 1", EntityType::Renderable, {}};
+    SceneTreeView subtree2 = {"Entity 2", EntityType::Camera, {std::make_shared<SceneTreeView>(subtree1)}};
+    SceneTreeView subtree = {"Entity 3", EntityType::LightSource, {}};
+    ui.setSceneTree({"Scene", EntityType::Scene, {std::make_shared<SceneTreeView>(subtree), std::make_shared<SceneTreeView>(subtree2)}});
+}
 
 bool Hierarchy::update() {
     ui.render();
