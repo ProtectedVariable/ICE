a86caaf8f8fdab4ed01c4218b46f87413563b879
Thomas Ibanez <thomas.ibanez@earlysight.com>
2023-10-31 09:22:27 +0100

    template dark magic

--- ICEBERG/UI/Widget.h
+++ ICEBERG/UI/Widget.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <any>
 #include <functional>
 #include <string>
 #include <unordered_map>
@@ -8,11 +9,45 @@ class Widget {
    public:
     virtual void render() = 0;
 
-    void registerCallback(const std::string& event, const std::function<void()>& callback) { m_callbacks.try_emplace(event, callback); }
-    void callback(const std::string& event) {
+    void registerCallback(const std::string &event, const std::function<void()> &callback) { m_callbacks.try_emplace(event, callback); }
+    void callback(const std::string &event) {
         if (m_callbacks.contains(event)) {
             m_callbacks[event]();
         }
+    }
+
+    template<typename F>
+    void store_lambda(const std::string &key, F &&f) {
+        using arg_tuple = typename function_traits<F>::arg_tuple;
+        using return_type = typename function_traits<F>::return_type;
+
+        myHash[key] = [f = std::forward<F>(f)](std::any args) -> void {
+            if constexpr (std::is_same_v<return_type, void>) {
+                std::apply(f, std::any_cast<arg_tuple &>(args));
+            }
+        };
+    }
+
+    template<typename... Args>
+    void call(const std::string &key, Args... arg) {
+        call_lambda(myHash[key], arg...);
+    }
+
+   private:
+    std::unordered_map<std::string, std::function<void(std::any)>> myHash;
+
+    template<typename T>
+    struct function_traits : public function_traits<decltype(&T::operator())> {};
+
+    template<typename ClassType, typename ReturnType, typename... Args>
+    struct function_traits<ReturnType (ClassType::*)(Args...) const> {
+        using arg_tuple = std::tuple<Args...>;
+        using return_type = ReturnType;
+    };
+
+    template<typename... Args>
+    void call_lambda(const std::function<void(std::any)> &f, Args &&...args) {
+        f(std::make_tuple(std::forward<Args>(args)...));
     }
 
    protected:
--- ICEBERG/src/Hierarchy.cpp
+++ ICEBERG/src/Hierarchy.cpp
@@ -1,10 +1,13 @@
 #include "Hierarchy.h"
 
+#include <iostream>
+
 Hierarchy::Hierarchy(const std::shared_ptr<ICE::ICEEngine>& engine) : m_engine(engine) {
     SceneTreeView subtree1 = {"Entity 1", EntityType::Renderable, {}};
     SceneTreeView subtree2 = {"Entity 2", EntityType::Camera, {std::make_shared<SceneTreeView>(subtree1)}};
     SceneTreeView subtree = {"Entity 3", EntityType::LightSource, {}};
     ui.setSceneTree({"Scene", EntityType::Scene, {std::make_shared<SceneTreeView>(subtree), std::make_shared<SceneTreeView>(subtree2)}});
+    ui.store_lambda("hierarchy_changed", [](std::string s1, std::string s2) { std::cout << s1 << s2 << std::endl; });
 }
 
 bool Hierarchy::update() {
