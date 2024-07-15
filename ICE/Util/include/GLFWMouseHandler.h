547dfb8bf9818a9434f61eb84438d3490f32697b
Thomas Ibanez <thomas.ibanez@earlysight.com>
2024-07-15 14:27:41 +0200

    add mouse button callbacks

--- ICE/Util/include/GLFWMouseHandler.h
+++ ICE/Util/include/GLFWMouseHandler.h
@@ -17,6 +17,30 @@ class GLFWMouseHandler : public MouseHandler {
                 f(xpos, ypos);
             }
         });
+
+        glfwSetMouseButtonCallback(m_ptr, [](GLFWwindow *window, int button, int action, int mods) {
+            GLFWWindow *w = (GLFWWindow *) glfwGetWindowUserPointer(window);
+            auto self = (GLFWMouseHandler *) w->getInputHandlers().first.get();
+            ButtonAction act = action == GLFW_PRESS ? ButtonAction::PRESS : ButtonAction::RELEASE;
+            MouseButton btn = MouseButton::OTHER_MOUSE_BUTTON;
+            switch (button) {
+                case GLFW_MOUSE_BUTTON_LEFT:
+                    btn = MouseButton::LEFT_MOUSE_BUTTON;
+                    break;
+                case GLFW_MOUSE_BUTTON_RIGHT:
+                    btn = MouseButton::RIGHT_MOUSE_BUTTON;
+                    break;
+                case GLFW_MOUSE_BUTTON_MIDDLE:
+                    btn = MouseButton::MIDDLE_MOUSE_BUTTON;
+                    break;
+                default:
+                    btn = MouseButton::OTHER_MOUSE_BUTTON;
+                    break;
+            }
+            for (const auto &f : self->m_mousebutton_callbacks) {
+                f(btn, act);
+            }
+        });
     }
 
     void setGrabMouse(bool grab) override {
--- ICE/Util/include/MouseHandler.h
+++ ICE/Util/include/MouseHandler.h
@@ -3,16 +3,23 @@
 #include <functional>
 
 namespace ICE {
-using MouseEventCallback = std::function<void(float x, float y)>;
+
+enum class MouseButton { LEFT_MOUSE_BUTTON, RIGHT_MOUSE_BUTTON, MIDDLE_MOUSE_BUTTON, OTHER_MOUSE_BUTTON };
+enum class ButtonAction { PRESS, RELEASE };
+
+using MouseMoveEventCallback = std::function<void(float x, float y)>;
+using MouseButtonEventCallback = std::function<void(MouseButton, ButtonAction)>;
 class MouseHandler {
- public:
-  virtual ~MouseHandler() = default;
+   public:
+    virtual ~MouseHandler() = default;
 
-  virtual void update() {}
-  virtual void setGrabMouse(bool grab) = 0;
-  void addMouseMoveListener(const MouseEventCallback& callback) { m_mousemove_callbacks.push_back(callback); }
+    virtual void update() {}
+    virtual void setGrabMouse(bool grab) = 0;
+    void addMouseMoveListener(const MouseMoveEventCallback& callback) { m_mousemove_callbacks.push_back(callback); }
+    void addMouseClickListener(const MouseButtonEventCallback& callback) { m_mousebutton_callbacks.push_back(callback); }
 
- protected:
-  std::vector<MouseEventCallback> m_mousemove_callbacks;
+   protected:
+    std::vector<MouseMoveEventCallback> m_mousemove_callbacks;
+    std::vector<MouseButtonEventCallback> m_mousebutton_callbacks;
 };
 }  // namespace ICE
