a5a8d4b381038f5aed919e923bcb8db4dce42850
Thomas Ibanez <thomas.ibanez@earlysight.com>
2024-12-11 16:21:00 +0100

    list files for debugging

--- ICE/IO/test/MeshLoaderTest.cpp
+++ ICE/IO/test/MeshLoaderTest.cpp
@@ -1,11 +1,13 @@
+#include <NoneGraphicsFactory.h>
 #include <gtest/gtest.h>
 
 #include "MeshLoader.h"
-#include <NoneGraphicsFactory.h>
 
 using namespace ICE;
 
 TEST(MeshLoaderTest, LoadFromObj) {
+    for (const auto &entry : std::filesystem::directory_iterator("."))
+        std::cout << entry.path() << std::endl;
     auto gr_f = std::make_shared<NoneGraphicsFactory>();
     auto mesh = MeshLoader(gr_f).load({"cube.obj"});
     EXPECT_EQ(mesh->getVertices().size(), 24);
