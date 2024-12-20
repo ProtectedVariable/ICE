64d877087918d589fddc9dccc1e3ce754ac342d8
Thomas Ibanez <thomas.ibanez@earlysight.com>
2024-12-20 15:43:33 +0100

    Create new entity is now a child popup from tree items

--- ICEBERG/UI/HierarchyWidget.h
+++ ICEBERG/UI/HierarchyWidget.h
@@ -60,7 +60,7 @@ class HierarchyWidget : public Widget {
                 }
                 ImGui::EndDragDropTarget();
             }
-            if (ImGui::BeginPopupContextWindow("hierarchy_popup")) {
+            if (ImGui::BeginPopupContextItem("hierarchy_popup")) {
                 if (ImGui::Button("Create new entity")) {
                     callback("create_entity_clicked", tree.id);
                     ImGui::CloseCurrentPopup();
