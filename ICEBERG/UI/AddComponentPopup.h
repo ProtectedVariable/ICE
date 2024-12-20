862398250d20909d6fe79d6a446859f209042903
ProtectedVariable <thomas.ibanez33@gmail.com>
2024-12-21 00:27:09 +0100

    only show available components in popup

--- ICEBERG/UI/AddComponentPopup.h
+++ ICEBERG/UI/AddComponentPopup.h
@@ -7,12 +7,21 @@
 
 class AddComponentPopup {
    public:
-    AddComponentPopup() : m_components_combo("##add_component_combo", {"Render", "Transform", "Light"}) {}
+    AddComponentPopup() : m_components_combo("##add_component_combo", {"Render", "Light"}) {}
 
     void open(const std::shared_ptr<ICE::Registry> &registry, ICE::Entity entity) {
         m_registry = registry;
         m_entity = entity;
         m_open = true;
+
+        std::vector<std::string> values;
+        if(!registry->entityHasComponent<ICE::RenderComponent>(entity)) {
+            values.push_back("Render Component");
+        }
+        if(!registry->entityHasComponent<ICE::LightComponent>(entity)) {
+            values.push_back("Light Component");
+        }
+        m_components_combo.setValues(values);
     }
 
     void render() {
@@ -25,20 +34,18 @@ class AddComponentPopup {
         if (ImGui::BeginPopupModal("Add Component", 0, ImGuiWindowFlags_AlwaysAutoResize)) {
             m_components_combo.render();
             if (ImGui::Button("Add")) {
-                switch (m_components_combo.getSelectedIndex()) {
-                    case 0:
-                        m_registry->addComponent(m_entity, ICE::RenderComponent(0, 0));
-                        break;
-                    case 1:
-                        m_registry->addComponent(
-                            m_entity, ICE::TransformComponent(Eigen::Vector3f(0, 0, 0), Eigen::Vector3f(0, 0, 0), Eigen::Vector3f(1, 1, 1)));
-                        break;
-                    case 2:
-                        m_registry->addComponent(m_entity, ICE::LightComponent(ICE::PointLight, Eigen::Vector3f(1, 1, 1)));
-                        break;
-                }
+                if(m_components_combo.getSelectedItem() == "Render Component")
+                    m_registry->addComponent(m_entity, ICE::RenderComponent(0, 0));
+                    
+                if(m_components_combo.getSelectedItem() == "Light Component")
+                    m_registry->addComponent(m_entity, ICE::LightComponent(ICE::PointLight, Eigen::Vector3f(1, 1, 1)));
                 ImGui::CloseCurrentPopup();
                 m_accepted = true;
+            }
+            ImGui::SameLine();
+            if(ImGui::Button("Cancel")) {
+                ImGui::CloseCurrentPopup();
+
             }
             ImGui::EndPopup();
         }
