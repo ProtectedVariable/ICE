4325cd6c67879fbc047755a1d479d149549654d7
Thomas Ibanez <thomas.ibanez@earlysight.com>
2025-01-08 10:14:30 +0100

    add bounding box to model

--- ICE/Graphics/include/Model.h
+++ ICE/Graphics/include/Model.h
@@ -10,6 +10,7 @@ class Model : public Asset {
 
     std::vector<std::shared_ptr<ICE::Mesh>> getMeshes() const { return m_meshes; }
     std::vector<ICE::AssetUID> getMaterialsIDs() const { return m_materials; }
+    AABB getBoundingBox() const { return m_boundingbox; }
 
     AssetType getType() const override { return AssetType::EModel; }
     std::string getTypeName() const override { return "Model"; }
@@ -17,5 +18,6 @@ class Model : public Asset {
    private:
     std::vector<std::shared_ptr<ICE::Mesh>> m_meshes;
     std::vector<ICE::AssetUID> m_materials;
+    AABB m_boundingbox{{0, 0, 0}, {0, 0, 0}};
 };
 }  // namespace ICE
--- ICE/Graphics/src/Model.cpp
+++ ICE/Graphics/src/Model.cpp
@@ -4,6 +4,9 @@ namespace ICE {
 Model::Model(const std::vector<std::shared_ptr<ICE::Mesh>> &meshes, const std::vector<ICE::AssetUID> &materials)
     : m_meshes(meshes),
       m_materials(materials) {
+    for (const auto &mesh : meshes) {
+        m_boundingbox = m_boundingbox.unionWith(mesh->getBoundingBox());
+    }
 }
 
 }  // namespace ICE
